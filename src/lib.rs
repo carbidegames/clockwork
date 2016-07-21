83b12cd4cabdee199383164ee304f5e70f69000f
Layl <LaylConway@users.noreply.github.com>
2016-07-21 14:52:36 +0200

    Change file handler to make use of starts_with and is_file instead

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,6 @@ extern crate route_recognizer;
 extern crate webutil;
 #[macro_use] extern crate try_opt;
 #[macro_use] extern crate mopa;
-extern crate regex;
 
 mod clockwork;
 mod listener;
--- src/routes/files.rs
+++ src/routes/files.rs
@@ -4,61 +4,29 @@ use std::io::Read;
 use modules::Modules;
 use routes::{RouteHandler, UrlParams};
 use webutil::HtmlString;
-use regex::Regex;
 
 pub fn file_handler<D: AsRef<Path>>(directory: D) -> FileHandler {
-    let regex = concat!(
-        // Control characters (0x00–0x1f and 0x80–0x9f)
-        r"(0x[0-1|8-9][0-f])|",
-        // Reserved characters (/, \, ?, <, >, :, *, |, and ")
-        r#"(/|\\|\?|<|>|:|\*|\||")|"#,
-        // A string consisting of or ending with only periods or whitespace
-        r"([.\s]+$)|",
-        // Windows reserved filenames
-        "(^(CON|PRN|AUX|NUL|COM1|COM2|COM3|COM4|COM5|COM6|COM7|COM8|COM9|LPT1|LPT2|LPT3|LPT4|LPT5",
-        "|LPT6|LPT7|LPT8|LPT9)$)"
-    );
-
     FileHandler {
-        section_regex: Regex::new(regex).unwrap(),
         directory: directory.as_ref().to_path_buf(),
     }
 }
 
 pub struct FileHandler {
-    section_regex: Regex,
     directory: PathBuf,
-}
-
-impl FileHandler {
-    fn is_section_valid(&self, section: &str) -> bool {
-        // Section can't be over 255 bytes
-        if section.len() > 255 {
-            return false;
-        }
-
-        // Section can't have a regex match
-        !self.section_regex.is_match(section)
-    }
 }
 
 impl RouteHandler for FileHandler {
     fn handle(&self, _: &Modules, url: UrlParams) -> Vec<u8> {
-        // We manually handle the path processing just to be sure, first start by splitting it
+        // Append the relative path to the root directory, then canonicalize it to weed out /../
         let path_param = url.get("").unwrap();
-        let sections: Vec<_> = path_param.split(|c| c == '\\' || c == '/').collect();
-
-        // Build up the path of the actual file we are looking for
         let mut path = self.directory.clone();
-        for section in sections.iter() {
-            // Make sure this specific section of the path is valid
-            if !self.is_section_valid(section) {
-                // TODO: Improve error handling
-                return HtmlString::bless("<h1>Invalid URL</h1>").into();
-            }
+        path.push(path_param);
+        let path = path.canonicalize().unwrap();
 
-            // Add it to the path
-            path.push(section);
+        // Make sure the path is still within the base directory and is pointing to a file
+        if !path.starts_with(&self.directory) && !path.is_file() {
+            // TODO: Improve error handling
+            return HtmlString::bless("<h1>Invalid URL</h1>").into()
         }
 
         // We've got a valid path, open up the file
